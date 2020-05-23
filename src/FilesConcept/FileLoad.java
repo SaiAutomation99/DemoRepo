67628d9f00558d94b6b3104592282a2e76d287d6
SaiAutomation99 <65777441+SaiAutomation99@users.noreply.github.com>
2020-05-23 15:42:37 +0530

    sysout

--- src/FilesConcept/FileLoad.java
+++ src/FilesConcept/FileLoad.java
@@ -22,6 +22,8 @@ public class FileLoad {
 		File f1=new File("D:\\Study\\ExtentReport\\Extentreporta.txt");
 		
 System.out.println("file no created we need to create using create new file");
+			System.out.println("file no created we need to create using create new file");
+
 		
 		f1.createNewFile();
 		}catch(Exception e) {
