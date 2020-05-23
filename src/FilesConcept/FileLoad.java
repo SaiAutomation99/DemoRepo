f34795fa167deeb28214c02b2c32a68a84f895a8
SaiAutomation99 <saiprasanth.qa@gmail.com>
2020-05-23 15:47:46 +0530

    sysout statement

--- src/FilesConcept/FileLoad.java
+++ src/FilesConcept/FileLoad.java
@@ -25,6 +25,8 @@ System.out.println("file no created we need to create using create new file");
 			System.out.println("file no created we need to create using create new file");
 
 					System.out.println("file no created we need to create using create new file");
+					System.out.println("file no created we need to create using create new file");
+
 
 		f1.createNewFile();
 		}catch(Exception e) {
